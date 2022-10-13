8abb0e231cce5fd5db9dec165bae1c2758ddda9d
TheRadest1 <18326@verona.k12.wi.us>
2022-10-12 23:12:40 -0500

    lots more bug fixes, and much smooth

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,10 @@
 setTimeout(() => {document.getElementById("loading").style.display = "none";}, 400);
 function slideOut(newHref) {
-	document.getElementById("navbar-left").style.animation = "transitionOutLeft 1s";
-	document.getElementById("firstname").style.animation = "transitionOutRight 1s";
-	document.getElementById("lastname").style.animation = "transitionOutRight 1s";
-	document.getElementById("loading").style.animation = "transitionInBottom .8s";
-	document.getElementById("loading").style.display = "block";
-	setTimeout(() => {location.href = newHref;}, 800);
-}
+	document.getElementById("navbar-left").style.animation = "transitionOutLeft 1.5s";
+	document.getElementById("firstname").style.animation = "transitionOutRight 1.5s";
+	document.getElementById("lastname").style.animation = "transitionOutRight 1.5s";
+	setTimeout(() => {document.getElementById("loading").style.animation = "transitionInBottom .8s";}, 300);
+	setTimeout(() => {document.getElementById("loading").style.display = "block";}, 300);
+	
+	setTimeout(() => {location.href = newHref;}, 1200);
+} 
--- public/js/skills.js
+++ public/js/skills.js
@@ -1,31 +1,33 @@
 setTimeout(() => {document.getElementById("loading").style.display = "none";}, 400);
+
 function slideOut() {
-	document.getElementById("hambar").style.animation = "transitionOutLeft 1s"
-	document.getElementById("title").style.animation = "transitionOutLeft 1s"
-	document.getElementById("loading").style.animation = "transitionInBottom .8s";
-	document.getElementById("loading").style.display = "block";
-	setTimeout(() => {location.href = '/';}, 800);
+	document.getElementById("hambar").style.animation = "transitionOutLeft 1.5s"
+	document.getElementById("title").style.animation = "transitionOutLeft 1.5s"
+	setTimeout(() => {document.getElementById("loading").style.animation = "transitionInBottom .8s";}, 300);
+	setTimeout(() => {document.getElementById("loading").style.display = "block";}, 300);
+	
+	setTimeout(() => {location.href = '/views/';}, 1200);
 }
 
 function toggleDiv(ID, displayType="block", slide = "none") {
-  var x = document.getElementById(ID);
-  if (x.style.display === displayType) {
-	if (slide == "right") {
-		x.style.animation = "transitionOutRight .5s"
-	}
-	else if (slide == "left") {
-		x.style.animation = "transitionOutLeft .5s"
-	}
+  	var x = document.getElementById(ID);
+  	if (x.style.display === displayType) {
+		if (slide == "right") {
+			x.style.animation = "transitionOutRight .5s ease-in"
+		}
+		else if (slide == "left") {
+			x.style.animation = "transitionOutLeft .5s ease-in"
+		}
 
-	setTimeout(() => {x.style.display = "none";}, 400);
-  } else {
-	if (slide == "right") {
-		x.style.animation = "transitionInRight .5s"
-	}
-	else if (slide == "left") {
-		x.style.animation = "transitionInLeft .5s"
-	}
+		setTimeout(() => {x.style.display = "none";}, 400);
+	} else {
+		if (slide == "right") {
+			x.style.animation = "transitionInRight .5s ease-out"
+		}
+		else if (slide == "left") {
+			x.style.animation = "transitionInLeft .5s ease-out"
+		}
 
-	x.style.display = displayType;
-  }
+		x.style.display = displayType;
+  	}
 }
