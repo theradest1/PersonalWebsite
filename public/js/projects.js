d0ebac8b0008c607d614ac13339825e072d26173
TheRadest1 <18326@verona.k12.wi.us>
2022-10-14 00:35:02 -0500

    PROJECTS ARE SO SMOOTH

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,8 +1,25 @@
 setTimeout(() => {document.getElementById("loading").style.display = "none";}, 400);
 
+setTimeout(() => {slideIn();}, 400);
+
+function slideIn() {
+	for(let i = 1; i <= 8; i++) {
+		const img = document.getElementById("img" + i).style;
+		img.display = "block";
+		img.animation = "transitionInBottomFar " + (i/10 + .5) + "s";
+	}
+}
+
 function slideOut() {
 	document.getElementById("title").style.animation = "transitionOutLeft 1.8s"
 	
+	for(let i = 1; i <= 8; i++) {
+		const img = document.getElementById("img" + i).style;
+		//img.display = "block";
+		img.animation = "transitionOutBottomFar " + ((8 - i)/10 + .8) + "s";
+		setTimeout(() => {img.display = "none";}, 800);
+	}
+
 	setTimeout(() => {document.getElementById("loading").style.animation = "transitionInBottom .8s";}, 800);
 	setTimeout(() => {document.getElementById("loading").style.display = "block";}, 800);
 	
