b83db93abf54d10562b021f4b3005064f3791214
TheRadest1 <18326@verona.k12.wi.us>
2022-10-15 23:06:40 -0500

    A few bug fixes, and a favicon (:

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,18 +1,32 @@
 setTimeout(() => {document.getElementById("loading").style.display = "none";}, 400);
-
 setTimeout(() => {slideIn();}, 400);
+
+let availableProjects = [0,1,2,3,4,5,6,7,8];
 
 function slideIn() {
 	for(let i = 1; i <= 8; i++) {
+		const num = getNewProjectID();
 		const img = document.getElementById("img" + i);
-		img.style.display = "block";
-		img.src = "../public/img/image" + Math.floor(Math.random() * 9) + ".png"
-		img.style.animation = "transitionInBottomFar " + (i/10 + .5) + "s";
+		img.src = "../public/img/image" + num + ".png"
+
+		setTimeout(() => {img.style.display = "block"; img.style.animation = "transitionInBottomFar " + (i/10 + .5) + "s";}, 400);
+		
 	}
+}
+
+function getNewProjectID(oldID = -1) {
+	const num = availableProjects[Math.floor(Math.random() * availableProjects.length)];
+	availableProjects.splice(availableProjects.indexOf(num), 1);
+	if(oldID != -1) {
+		availableProjects.push(oldID);
+	}
+	console.log(availableProjects);
+	return num;
 }
 
 function slideOut() {
 	document.getElementById("title").style.animation = "transitionOutLeft 1.8s"
+	
 	
 	for(let i = 1; i <= 8; i++) {
 		const img = document.getElementById("img" + i);
@@ -27,8 +41,16 @@ function slideOut() {
 }
 
 async function changeImage(ID, direction, direction2) {
-	const dir = [direction, direction2][Math.floor(Math.random() * 2)];
 	const img = document.getElementById(ID)
+	const link = img.parentElement.onclick;
+	img.parentElement.onclick = "";
+	const dir = [direction, direction2][Math.floor(Math.random() * 2)];
+
 	img.style.animation = "transitionOut" + dir + " 1s";
-	setTimeout(() => {img.style.animation = "transitionIn" + dir + " 1s"; img.src = "../public/img/image" + Math.floor(Math.random() * 8) + ".png";}, 900);
+
+	const pastImg = parseInt(img.src.substring(38, 45));
+	console.log(img.src.substring(38, 45));
+	const num = getNewProjectID(pastImg);
+	setTimeout(() => {img.style.animation = "transitionIn" + dir + " 1s"; img.src = "../public/img/image" + num + ".png";}, 900);
+	setTimeout(() => {img.parentElement.onclick = link;}, 1800);
 }
