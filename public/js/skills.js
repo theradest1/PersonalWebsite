52dfba4a17dbefa6e8f77c7ebef37a06a0acd1c5
TheRadest1 <18326@verona.k12.wi.us>
2022-10-17 12:32:42 -0500

    small skill fix

--- public/js/skills.js
+++ public/js/skills.js
@@ -1,7 +1,10 @@
 setTimeout(() => {document.getElementById("loading").style.display = "none";}, 400);
 
+var openSkillID = "none";
+
 function slideOut() {
-	document.getElementById("hambar").style.animation = "transitionOutLeft 1.8s"
+	document.getElementById("hambar-list").style.animation = "transitionOutLeft 1.8s"
+	document.getElementById("skill-list-container").style.animation = "transitionOutRightFar 1.8s"
 	document.getElementById("title").style.animation = "transitionOutLeft 1.8s"
 	setTimeout(() => {document.getElementById("loading").style.animation = "transitionInBottom .8s";}, 800);
 	setTimeout(() => {document.getElementById("loading").style.display = "block";}, 800);
@@ -9,25 +12,26 @@ function slideOut() {
 	setTimeout(() => {location.href = '/views/';}, 1600);
 }
 
-function toggleDiv(ID, displayType="block", slide = "none") {
+function toggleSkill(ID, displayType="block") {
   	var x = document.getElementById(ID);
-  	if (x.style.display === displayType) {
-		if (slide == "right") {
-			x.style.animation = "transitionOutRight .5s ease-in"
-		}
-		else if (slide == "left") {
-			x.style.animation = "transitionOutLeft .5s ease-in"
-		}
+  	if (x.style.display == "block") {
+		x.style.animation = "transitionOutRight .5s ease-in"
 
+		openSkillID = "none";
 		setTimeout(() => {x.style.display = "none";}, 400);
 	} else {
-		if (slide == "right") {
+		var timeout = 1;
+		if(openSkillID != "none"){
+			timeout = 500;
+			document.getElementById(openSkillID).style.animation = "transitionOutRight .5s ease-in"
+			setTimeout(() => {document.getElementById(openSkillID).style.display = "none";}, 400);
+		}
+		
+		setTimeout(() => {
 			x.style.animation = "transitionInRight .5s ease-out"
-		}
-		else if (slide == "left") {
-			x.style.animation = "transitionInLeft .5s ease-out"
-		}
-
-		x.style.display = displayType;
+			openSkillID = ID;
+			x.style.display = displayType;
+		}, timeout);
+		
   	}
 }
