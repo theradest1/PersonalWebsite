c94fccd4c36cd36ef84e640bf984419447f92ca6
TheRadest1 <18326@verona.k12.wi.us>
2022-10-27 11:57:38 -0500

    worked on project demos

--- public/js/project-demos.js
+++ public/js/project-demos.js
@@ -1,5 +1,9 @@
 setTimeout(() => {document.getElementById("loading").style.display = "none";}, 300);
 
+function showProject(projectURL) {
+	document.getElementById("projectIframe").src = projectURL;
+}
+
 function slideOut(newHref) {
 	document.getElementById("navbar-left").style.animation = "transitionOutLeft 1.8s";
 	setTimeout(() => {document.getElementById("loading").style.animation = "transitionInBottom .8s";}, 800);
