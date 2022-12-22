e5cfdd31031d4d1e041fd700a059653fc79bb851
TheRad1 <18326@verona.k12.wi.us>
2022-12-22 00:46:44 -0600

    small changes

--- public/js/skills.js
+++ public/js/skills.js
@@ -14,25 +14,28 @@ function slideOut() {
 }
 
 function toggleSkill(ID, displayType="block") {
-  	var x = document.getElementById(ID);
-  	if (x.style.display == "block") {
-		x.style.animation = "transitionOutRight .5s ease-in"
+	if(openSkillID != ID){
 
-		openSkillID = "none";
-		setTimeout(() => {x.style.display = "none";}, 400);
-	} else {
-		var timeout = 1;
-		if(openSkillID != "none"){
-			timeout = 500;
-			document.getElementById(openSkillID).style.animation = "transitionOutRight .5s ease-in"
-			setTimeout(() => {document.getElementById(openSkillID).style.display = "none";}, 400);
+		var x = document.getElementById(ID);
+		if (x.style.display == "block") {
+			x.style.animation = "transitionOutRight .5s ease-in"
+
+			openSkillID = "none";
+			setTimeout(() => {x.style.display = "none";}, 400);
+		} else {
+			var timeout = 1;
+			if(openSkillID != "none"){
+				timeout = 500;
+				document.getElementById(openSkillID).style.animation = "transitionOutRight .5s ease-in"
+				setTimeout(() => {document.getElementById(openSkillID).style.display = "none";}, 400);
+			}
+			
+			setTimeout(() => {
+				x.style.animation = "transitionInRight .5s ease-out"
+				openSkillID = ID;
+				x.style.display = displayType;
+			}, timeout);
+			
 		}
-		
-		setTimeout(() => {
-			x.style.animation = "transitionInRight .5s ease-out"
-			openSkillID = ID;
-			x.style.display = displayType;
-		}, timeout);
-		
-  	}
+	}
 }
