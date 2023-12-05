068b19db34ed44ab5fb04fa6a093ef68e17816fb
TheRad1 <landon.bakken@gmail.com>
2023-12-04 23:21:22 -0600

    added UM2 example

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = 4000;
 const host = "0.0.0.0";
 
 app.use(express.static("public"));
-app.use("/css", express.static(__dirname + 'public/css'));
-app.use("/js", express.static(__dirname + 'public/js'));
-app.use("/img", express.static(__dirname + 'public/img'));
-app.use("/files", express.static(__dirname + 'public/files'));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/img", express.static(__dirname + "public/img"));
+app.use("/files", express.static(__dirname + "public/files"));
+app.use("/game", express.static(__dirname + "public/game"));
 
-
-app.set('views', './views');
+app.set("views", "./views");
 
 app.get("", (req, res) => {
 	res.sendFile("/views/index.html", { root: __dirname });
@@ -34,31 +35,31 @@ app.get("/project-demos", (req, res) => {
 	res.sendFile("/views/project-demos.html", { root: __dirname });
 });
 app.get("/aplus", (req, res) => {
-	res.sendFile("/views/aplus.html", { root: __dirname })
+	res.sendFile("/views/aplus.html", { root: __dirname });
 });
 app.get("/raffle/entry/:raffleID", (req, res) => {
-	res.sendFile("/views/raffle/entry.html", { root: __dirname })
+	res.sendFile("/views/raffle/entry.html", { root: __dirname });
 });
 app.get("/raffle/results", (req, res) => {
-	res.sendFile("/views/raffle/results.html", { root: __dirname })
+	res.sendFile("/views/raffle/results.html", { root: __dirname });
 });
 app.get("/raffle/lookup/:raffleID", (req, res) => {
-	res.sendFile("/views/raffle/lookup.html", { root: __dirname })
+	res.sendFile("/views/raffle/lookup.html", { root: __dirname });
 });
 app.get("/games", (req, res) => {
-	res.sendFile("/views/games.html", { root: __dirname })
+	res.sendFile("/views/games.html", { root: __dirname });
 });
 
 //downloads
 app.get("/games/surge/updater", (req, res) => {
-	res.sendFile("/public/files/surge/surgeUpdater.zip", { root: __dirname })
+	res.sendFile("/public/files/surge/surgeUpdater.zip", { root: __dirname });
 });
 app.get("/games/surge/server", (req, res) => {
-	res.sendFile("/public/files/surge/surgeServer.zip", { root: __dirname })
+	res.sendFile("/public/files/surge/surgeServer.zip", { root: __dirname });
 });
 /*app.use(function (req, res) {
 	res.status(404).sendFile('/views/pageNotFound.html', { root: __dirname });
-});*/ // stops url perameters from working 
+});*/ // stops url perameters from working
 
 app.listen(port, host, () => {
 	console.log("Server started on port " + port);
